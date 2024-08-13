9bd042e41ead73bc3dcf1e38bb1fdf43f06ff4fb
Kush <kuriaisac@gmail.com>
2024-08-13 16:41:01 +0300

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -15,7 +15,7 @@ function App() {
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button onClick={displayAppColor}>{darkMode ? "Dark mode" : "Light Mode"}</button>
+        <button onClick={displayAppColor}>{darkMode ? "Dark Mode" : "Light Mode"}</button>
       </header>
       <ShoppingList items={itemData} />
     </div>
